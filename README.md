2147e146411cc4f9eca6d456e9b6b0f37fe69004
Sou Syouzui <sou_s@worksap.co.jp>
2018-02-01 18:14:56 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# document-dom-renderer
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { h, render } from 'preact';
+import App from './App';
+
+let mountNode = document.getElementById('root') as Element;
+mountNode = render(<App />, mountNode, mountNode.lastElementChild || undefined);
+
+if (process.env.NODE_ENV !== 'production') {
+  require('preact/debug');
+}
