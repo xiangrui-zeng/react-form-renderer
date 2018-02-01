528123fc8034fd2f6c402264a88f362ae172e60b
Yang LIU <liu_y@worksap.co.jp>
2018-02-01 16:58:03 +0900

    Add boilerplate for the app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { h, Component } from 'preact';
+
+export default class App extends Component<{}, {}> {
+  public render(): JSX.Element {
+    return (
+      <div>Hello world!</div>
+    );
+  }
+}
